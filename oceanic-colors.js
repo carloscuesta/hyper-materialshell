86ea63b45e46f7785cd96e3532e76cc20b04b708
Nicolas Marien <nmarien29@gmail.com>
2016-12-24 17:39:30 +0100

    :sparkles: Add oceanic version of materialshell. (#8)
    Closes #7

---
+++ oceanic-colors.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = {
+	palette: {
+		black: '#252525',
+		red: '#FF5252',
+		green: '#C3D82C',
+		yellow: '#FFD740',
+		blue: '#40C4FF',
+		magenta: '#FF4081',
+		cyan: '#18FFFF',
+		white: '#F5F5F5',
+		lightBlack: '#708284',
+		lightRed: '#FF5252',
+		lightGreen: '#C3D82C',
+		lightYellow: '#FFD740',
+		lightBlue: '#40C4FF',
+		lightMagenta: '#FF4081',
+		lightCyan: '#18FFFF',
+		lightWhite: '#F5F5F5'
+	},
+	background: '#263238'
+};
---
+++ regular-colors.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = {
+	palette: {
+		black: '#252525',
+		red: '#FF5252',
+		green: '#C3D82C',
+		yellow: '#FFC135',
+		blue: '#42A5F5',
+		magenta: '#D81B60',
+		cyan: '#00ACC1',
+		white: '#F5F5F5',
+		lightBlack: '#708284',
+		lightRed: '#FF5252',
+		lightGreen: '#C3D82C',
+		lightYellow: '#FFC135',
+		lightBlue: '#42A5F5',
+		lightMagenta: '#D81B60',
+		lightCyan: '#00ACC1',
+		lightWhite: '#F5F5F5'
+	},
+	background: '#151515'
+};
