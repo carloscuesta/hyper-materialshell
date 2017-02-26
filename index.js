471823d172ad55cb1fe30b04b1458f42f6ce05ad
Carlos Cuesta <carloscuesta@me.com>
2017-02-26 11:40:08 +0100

    :hammer: Refactor index.js

--- index.js
+++ index.js
@@ -5,16 +5,21 @@ const borderColor = '#252525';
 const oceanicColors = require('./colorschemes/oceanic.js');
 const defaultColors = require('./colorschemes/regular.js');
 
-exports.decorateConfig = config => {
-	let colors;
-	if (config.materialshell && config.materialshell.theme === 'oceanic') {
-		colors = oceanicColors;
-	} else {
-		colors = defaultColors;
+const getColorScheme = cfg => {
+	if (cfg.materialshell) {
+		switch (cfg.materialshell.theme) {
+			case 'oceanic': return oceanicColors;
+			case 'dark': return defaultColors;
+			default: return defaultColors;
+		}
 	}
+};
+
+exports.decorateConfig = config => {
+	const colors = getColorScheme(config);
 	const backgroundColor = colors.background;
 	return Object.assign({}, config, {
-		cursorColor: colors.red,
+		cursorColor: colors.palette.red,
 		cursorShape: 'UNDERLINE',
 		foregroundColor,
 		backgroundColor,
@@ -22,7 +27,7 @@ exports.decorateConfig = config => {
 		css: `${config.css || ''}
 		.tab_tab:before {border-left: 1px solid;}
 		.tab_active {background: rgba(255,255,255,0.05);}
-		.tab_active:before {border-color: ${colors.red};}
+		.tab_active:before {border-color: ${colors.palette.red};}
 		`,
 		colors: colors.palette
 	});
